739f637ec5c978f4627abbab850ead8d173fe18c
= <david.jacewicz@student.tdsb.on.ca>
2018-06-10 18:19:46 +0000

    can't access global variables

--- assemble.py
+++ assemble.py
@@ -1,11 +1,11 @@
 #!/bin/python
 from subprocess import call,check_output
 call(['./compile'])
-call(['strip','-x','m'])
-for s in ('.dynamic','.rela.dyn','.dynsym','.interp','.bss','.dynstr','.note.gnu.build-id','.eh_frame','.eh_frame_hdr','.gnu.hash','.comment'):
-	call(['strip','-R',s,'m'])
+# call(['strip','-x','m'])
+# for s in ('.dynamic','.rela.dyn','.dynsym','.interp','.bss','.dynstr','.note.gnu.build-id','.eh_frame','.eh_frame_hdr','.gnu.hash','.comment'):
+# 	call(['strip','-R',s,'m'])
 with open('m.s','w') as f:
-	f.write('.section .text\na:\n.octa ')
+	f.write('.section .data\na:\n.octa ')
 	b=check_output(['./encode.py','m'])
 	f.write(str(b)[2:-1]+"\n.global _start\n_start:\nmov $a,%rax\nadd $");
 	c=str(check_output(['readelf','-e','m']))
--- m.cpp
+++ m.cpp
@@ -11,16 +11,14 @@
 #define ns namespace
 
 #include"base/c"
-#include"wrap/h"
 struct ostream;
 struct istream;
 #include"int/h"
 #include"char/h"
 #include"chunk/h"
 
+#include"stacked/c"
 
-
-#include"wrap/c"
 #include"fstream/c"
 #include"ostream/c"
 #include"istream/c"
@@ -31,50 +29,53 @@ struct istream;
 #define op operator
 #define order op<
 
-tm<tn A>
-void merge(chunk<A> arr,u a,u b,u c) {
-	chunk<A> buf = mem(A,arr.size);
-	int i = a, j = b, bi = 0;
-	for(; i < b && j < c ;)
-		if(arr[i]<arr[j])
-			buf[bi++]= arr[i++];
-		else
-			buf[bi++]= arr[j++];
-	for(; i < b; ++i) buf[bi++] = arr[i];
-	for(; j < c; ++j) buf[bi++] = arr[j];
-	for(int k = 0; k < bi; ++k) arr[k+a] = buf[k];
-}
-tm<tn A>
-void mergesort(chunk<A> arr) {
-	for(u i = 1; i*2 <= arr.size; i *= 2)
-		for(u j = 0; j+i*2 <= arr.size; j += i*2)
-			merge(arr,j,j+i,j+i*2);
-	for(u j = arr.size - arr.size.lsb(),
-			i = j - j.lsb();
-			j != 0; j = i, i -= i.lsb()
-		 )
-		merge(arr,i,j,arr.size);
-}
-tm<tn A>
-void sort(chunk<A> a) {
-	mergesort(a);
-}
-struct mud {
-	u i,t;
-};
-bool order(mud a,mud b) {
-	return a.i<b.i; }
+// tm<tn A>
+// void merge(chunk<A> arr,u a,u b,u c) {
+// 	chunk<A> buf = mem(A,arr.size);
+// 	int i = a, j = b, bi = 0;
+// 	for(; i < b && j < c ;)
+// 		if(arr[i]<arr[j])
+// 			buf[bi++]= arr[i++];
+// 		else
+// 			buf[bi++]= arr[j++];
+// 	for(; i < b; ++i) buf[bi++] = arr[i];
+// 	for(; j < c; ++j) buf[bi++] = arr[j];
+// 	for(int k = 0; k < bi; ++k) arr[k+a] = buf[k];
+// }
+// tm<tn A>
+// void mergesort(chunk<A> arr) {
+// 	for(u i = 1; i*2 <= arr.size; i *= 2)
+// 		for(u j = 0; j+i*2 <= arr.size; j += i*2)
+// 			merge(arr,j,j+i,j+i*2);
+// 	for(u j = arr.size - arr.size.lsb(),
+// 			i = j - j.lsb();
+// 			j != 0; j = i, i -= i.lsb()
+// 		 )
+// 		merge(arr,i,j,arr.size);
+// }
+// tm<tn A>
+// void sort(chunk<A> a) {
+// 	mergesort(a);
+// }
+// struct mud {
+// 	u i,t;
+// };
+// bool order(mud a,mud b) {
+// 	return a.i<b.i; }
 asm(".globl _start");
 extern "C"
 void _start() {
-	ostream out(1,mem(Char,40000));
-	istream in(0,mem(Char,40000));
-	out("check" nl); out.flush(); exit(0);
-	auto[n,m,j]= in.get<u,u,u>();
-	auto muds= mem(mud,m);
-	for(u i=0; i<m; ++i)
-		std::tie(muds[i].i,muds[i].t)= in.get<u,u>();
-	sort(muds);
-	out.flush();
+	init_sbrk();
+	istream in(0,40000);
+	ostream out(1,40000);
+	write(1,(void*)"henlo" nl,6);
+	//out("test" nl);
+	// out("check" nl); out.flush(); exit(0);
+	// auto[n,m,j]= in.get<u,u,u>();
+	// auto muds= mem(mud,m);
+	// for(u i=0; i<m; ++i)
+	// 	std::tie(muds[i].i,muds[i].t)= in.get<u,u>();
+	// sort(muds);
+	// out.flush();
 	exit(0);
 }
